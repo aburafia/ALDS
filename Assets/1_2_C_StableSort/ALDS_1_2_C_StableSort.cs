eb33b89ef90a0fc50fe3d816468864b58aca29ac
龍利典 <t.ryu@payke.co.jp>
2020-11-04 01:24:04 +0900

    finish is stable sort

--- Assets/1_2_C_StableSort/ALDS_1_2_C_StableSort.cs
+++ Assets/1_2_C_StableSort/ALDS_1_2_C_StableSort.cs
@@ -8,10 +8,9 @@ public class ALDS_1_2_C_StableSort : MonoBehaviour
     RectTransform obj;
     Text lableText;
 
-    RectTransform[] objs;
-
-    float yCenter = 0;
-
+    RectTransform[] objsOrig = new RectTransform[10];
+    RectTransform[] objs = new RectTransform[10];
+    RectTransform[] objs2 = new RectTransform[10];
 
     // Start is called before the first frame update
     IEnumerator Start()
@@ -19,14 +18,14 @@ public class ALDS_1_2_C_StableSort : MonoBehaviour
         obj = GameObject.Find("Panel").GetComponent<RectTransform>();
         lableText = GameObject.Find("Text").GetComponent<Text>();
 
-        yCenter = GetComponent<RectTransform>().sizeDelta.y / 2;
+        var yCenter = GetComponent<RectTransform>().sizeDelta.y / 2;
+        obj.position = new Vector3(obj.position.x, yCenter, 0);
 
+        yield return StartCoroutine(genRandom(10));
+        yield return StartCoroutine(bubbleSort(objs));
+        yield return StartCoroutine(selectSort(objs2));
 
-        int count = 10;
-
-        objs = new RectTransform[count];
-        yield return StartCoroutine(genRandom(count));
-        yield return StartCoroutine(selectSort());
+        isStable(objs, objs2);
     }
 
     IEnumerator insertionSoftFunc()
@@ -36,12 +35,26 @@ public class ALDS_1_2_C_StableSort : MonoBehaviour
 
     IEnumerator genRandom(int count)
     {
+        var markColor = new Color[] { Color.red, Color.yellow, Color.cyan, Color.green };
+        var markName = new string[] { "R", "Y", "C", "G" };
+
         for (int i = 0; i < count; i++)
         {
+            var num = Random.Range(0, 13);
+            var mark = Random.Range(0, 4);
+
             objs[i] = clone(obj);
 
-            objs[i].position = numToPos(i);
-            objs[i].sizeDelta += new Vector2(0, Random.Range(0f, 300f));
+            objs[i].position = numToPos(i, objs);
+            objs[i].position += new Vector3(0, num * 10,0);
+
+            objs[i].GetComponent<Image>().color = markColor[mark];
+            objs[i].GetComponentInChildren<Text>().text = markName[mark] + num.ToString();
+
+            objs2[i] = clone(objs[i]);
+            objs2[i].position -= new Vector3(0, 200, 0);
+            objsOrig[i] = clone(objs[i]);
+            objsOrig[i].position += new Vector3(0, 200, 0);
 
             yield return new WaitForSeconds(0.1f);
         }
@@ -50,7 +63,38 @@ public class ALDS_1_2_C_StableSort : MonoBehaviour
 
     int count = 0;
 
-    IEnumerator selectSort()
+    IEnumerator bubbleSort(RectTransform[] objs)
+    {
+        int head = 0;
+        bool flg = true;
+        while (flg)
+        {
+            flg = false;
+
+            for (int i = objs.Length - 1; i > head; i--)
+            {
+                if (objs[i].position.y < objs[i - 1].position.y)
+                {
+                    var tmp = objs[i];
+                    objs[i] = objs[i - 1];
+                    objs[i - 1] = tmp;
+
+                    objs[i - 1].position = numToPos(i - 1,objs);
+                    objs[i].position = numToPos(i, objs);
+
+                    flg = true;
+
+                    yield return new WaitForSeconds(0.2f);
+                }
+            }
+
+            head++;
+        }
+
+        yield return null;
+    }
+
+    IEnumerator selectSort(RectTransform[] objs)
     {
         for (int i=0;i<objs.Length; i++) 
         {
@@ -58,29 +102,37 @@ public class ALDS_1_2_C_StableSort : MonoBehaviour
 
             for (int j = i; j < objs.Length; j++)
             {
-                min = objs[min].sizeDelta.y > objs[j].sizeDelta.y ? j : min;
+                min = objs[min].position.y > objs[j].position.y ? j : min;
             }
 
             var tmp = objs[i];
             objs[i] = objs[min];
             objs[min] = tmp;
 
-            objs[min].position = numToPos(min);
-            objs[i].position = numToPos(i);
-
-            count++;
-            lableText.text = count.ToString();
+            objs[min].position = numToPos(min, objs);
+            objs[i].position = numToPos(i, objs);
 
             yield return new WaitForSeconds(0.2f);
-
-            objs[i].GetComponent<Image>().color = Color.red;
         }
 
         yield return null;
     }
 
+    void isStable(RectTransform[] objs, RectTransform[] objs2)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if(objs[i].GetComponentInChildren<Text>().text != objs2[i].GetComponentInChildren<Text>().text)
+            {
+                lableText.text = "not stable";
+                return;
+            }
+            lableText.text = "stable";
+        }
+    }
+
     //添字から位置を導出
-    public Vector3 numToPos(int i)
+    public Vector3 numToPos(int i, RectTransform[] objs)
     {
         //間隔
         float dXPosition = 50f;
@@ -89,7 +141,7 @@ public class ALDS_1_2_C_StableSort : MonoBehaviour
         float firstXpos = 100;
 
 
-        return new Vector3(firstXpos + dXPosition * i, yCenter, 0) ;
+        return new Vector3(firstXpos + dXPosition * i, objs[i].position.y, 0) ;
     }
 
     public RectTransform clone(RectTransform go)
@@ -99,7 +151,6 @@ public class ALDS_1_2_C_StableSort : MonoBehaviour
 
         var clone = cloneGo.GetComponent<RectTransform>();
         clone.position = go.position;
-        clone.sizeDelta = go.sizeDelta;
         return clone;
     }
 }
